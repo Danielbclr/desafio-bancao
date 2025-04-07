011b6ec52152ccf4ba90f9c41ac9b3b1096a146c
Danielbclr <danielbclramos@gmail.com>
2025-04-06 21:57:18 -0300

    Setup inicial
    Feito setup inicial com Spring Initializr

---
+++ src/main/java/com/danbramos/desafio_bancao/DesafioBancaoApplication.java
@@ -0,0 +1,13 @@
+package com.danbramos.desafio_bancao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBancaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBancaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danbramos/desafio_bancao/DesafioBancaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danbramos.desafio_bancao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBancaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
